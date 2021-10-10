82c5e45c2f3266836ad9acba02770379c6874696
zhaorong <zhaorong@kuaishou.com>
2021-10-10 17:46:17 +0800

    feat: add file

--- problems/001.two-sum.js
+++ problems/001.two-sum.js
@@ -4,20 +4,20 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
-    const map = Object.create({});
-    for (let i = 0; i < nums.length; i++) {
-        //[subtract, index] 记录差值 && index
-        map[nums[i]] = [target - nums[i], i];
+const twoSum = function (nums, target) {
+  const map = Object.create({});
+  for (let i = 0; i < nums.length; i++) {
+    //[subtract, index] 记录差值 && index
+    map[nums[i]] = [target - nums[i], i];
+  }
+  for (let i = 0; i < nums.length; i++) {
+    const subtract = target - nums[i];
+    const index = map[subtract][1];
+    if (map.hasOwnProperty(subtract) && index !== i) {
+      return [i, index];
     }
-    for (let i = 0; i < nums.length; i++) {
-        const subtract = target - nums[i];
-        const index = map[subtract][1];
-        if (map.hasOwnProperty(subtract) && index !== i) {
-            return [i, index];
-        }
-    }
-    return [-1, -1];
+  }
+  return [-1, -1];
 };
 
-console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3, 2, 4], 6));
